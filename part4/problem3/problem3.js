22c778a0a5e99050f4854d699945ff15769ce43b
unknown <4friizal@gmail.com>
2022-07-01 08:07:31 +0700

    push problem 3, join dupliJoin Array Remove Duplicate

---
+++ part4/problem3/problem3.js
@@ -0,0 +1,26 @@
+function joinArrayRemoveDuplicate(arrayA, arrayB) {
+  let isiArrayA = arrayA.slice("");
+  let isiArrayB = arrayB.slice("");
+  // gabungan array A ke array B
+  let oneArray = isiArrayA.concat(isiArrayB);
+  // console.log(oneArray);
+
+  // kondisi yang belom selesai
+  let arr = [];
+  for (let i = 0; i < oneArray.length; i++) {
+    if (arr.indexOf(oneArray[i]) === -1) {
+      arr.push(oneArray[i]);
+    }
+  }
+
+  return arr;
+}
+// Test cases
+console.log(joinArrayRemoveDuplicate(["apel", "anggur"], ["lemon", "leci", "nanas"]))
+// ["apel", "anggur", "lemon", "leci", "nanas"]
+ 
+console.log(joinArrayRemoveDuplicate(["samsung", "apple"], ["apple", "sony", "xiaomi"]))
+// ["samsung", "apple", "sony", "xiaomi"]
+ 
+console.log(joinArrayRemoveDuplicate(["football", "basketball"], ["basketball", "football"]))
+// [“football”, “basketball”]
